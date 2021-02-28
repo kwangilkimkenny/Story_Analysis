286894a33900349f924a1d581520e45e6d09744e
kwangilKyle <38448277+kwangilkimkenny@users.noreply.github.com>
2021-03-01 06:23:28 +0900

    get result in 3min. final version

--- CHARACTER_/character_web_fin/ai_mbti_analysis.py
+++ CHARACTER_/character_web_fin/ai_mbti_analysis.py
@@ -62,7 +62,6 @@ def get_types(row):
     return pd.Series( {'IE':I, 'NS':N , 'TF': T, 'JP': J })
 
 
-
 #딕셔너리파일 설정
 b_Pers = {'I':0, 'E':1, 'N':0, 'S':1, 'F':0, 'T':1, 'J':0, 'P':1}
 
@@ -77,6 +76,7 @@ def translate_personality(personality):
     return [b_Pers[l] for l in personality]
 
 
+
 def translate_back(personality):
     # transform binary vector to mbti personality
     
@@ -84,7 +84,6 @@ def translate_back(personality):
     for i, l in enumerate(personality):
         s += b_Pers_list[i][l]
     return s
-
 
 # We want to remove these from the psosts
 unique_type_list = ['INFJ', 'ENTP', 'INTP', 'INTJ', 'ENTJ', 'ENFJ', 'INFP', 'ENFP',
@@ -108,7 +107,8 @@ def pre_process_data(data, remove_stop_words=True, remove_mbti_profiles=True):
     
     for row in data.iterrows():
         i+=1
-     
+        if (i % 500 == 0 or i == 1 or i == len_data):
+            print("%s of %s rows" % (i, len_data))
 
         ##### Remove and clean comments
         posts = row[1].posts
@@ -133,95 +133,69 @@ def pre_process_data(data, remove_stop_words=True, remove_mbti_profiles=True):
     return list_posts, list_personality
 
 
+# read data
+# data = pd.read_csv('/Users/jongphilkim/Desktop/Django_WEB/essayfitaiproject_2020_12_09/essayai/mbti_1.csv') 
+data = pd.read_csv('./mbti/mbti_1.csv') 
+
+# get_types 함수 적용
+data = data.join(data.apply (lambda row: get_types (row),axis=1))
+
+# load
+with open('./mbti/list_posts.pickle', 'rb') as f:
+    list_posts = pickle.load(f)
+
+# load
+with open('./mbti/list_personality.pickle', 'rb') as f:
+    list_personality = pickle.load(f)
+
+
+# # Posts to a matrix of token counts
+cntizer = CountVectorizer(analyzer="word", 
+                            max_features=1500, 
+                            tokenizer=None,    
+                            preprocessor=None, 
+                            stop_words=None,  
+                            max_df=0.7,
+                            min_df=0.1) 
+
+# Learn the vocabulary dictionary and return term-document matrix
+print("CountVectorizer...")
+X_cnt = cntizer.fit_transform(list_posts)
+
+
+#################################################
+#save!!! model X_cnt
+import pickle
+# save
+# with open('./essayai/ai_character/mbti/data_X_cnt.pickle', 'wb') as f:
+#     pickle.dump(X_cnt, f, pickle.HIGHEST_PROTOCOL)
+
+
+# load
+with open('./mbti/data_X_cnt.pickle', 'rb') as f:
+    X_cnt = pickle.load(f)
+
+#################################################
+
+# Transform the count matrix to a normalized tf or tf-idf representation
+tfizer = TfidfTransformer()
+
+print("Tf-idf...")
+# Learn the idf vector (fit) and transform a count matrix to a tf-idf representation
+X_tfidf =  tfizer.fit_transform(X_cnt).toarray()
+
+# load
+with open('./mbti/data.pickle', 'rb') as f:
+    X_tfidf = pickle.load(f)
 
 def mbti_classify(text):
-
-    # read data
-    # data = pd.read_csv('/Users/jongphilkim/Desktop/Django_WEB/essayfitaiproject_2020_12_09/essayai/mbti_1.csv') 
-    data = pd.read_csv('./essayai/data/mbti_1.csv') 
-
-    # get_types 함수 적용
-    data = data.join(data.apply (lambda row: get_types (row),axis=1))
-
-
-
-    list_posts, list_personality  = pre_process_data(data, remove_stop_words=True, remove_mbti_profiles=True)
-
-    # save
-    with open('./essayai/ai_character/mbti/list_posts.pickle', 'wb') as f:
-        pickle.dump(list_posts, f, pickle.HIGHEST_PROTOCOL)
-        
-        # save
-    with open('./essayai/ai_character/mbti/list_personality.pickle', 'wb') as f:
-        pickle.dump(list_personality, f, pickle.HIGHEST_PROTOCOL)
     
-
-    # Posts to a matrix of token counts
-    cntizer = CountVectorizer(analyzer="word", 
-                                max_features=1500, 
-                                tokenizer=None,    
-                                preprocessor=None, 
-                                stop_words=None,  
-                                max_df=0.7,
-                                min_df=0.1) 
-
-    # Learn the vocabulary dictionary and return term-document matrix
-    #print("CountVectorizer...")
-    X_cnt = cntizer.fit_transform(list_posts)
-
-    # Transform the count matrix to a normalized tf or tf-idf representation
-    tfizer = TfidfTransformer()
-
-    #print("Tf-idf...")
-    # Learn the idf vector (fit) and transform a count matrix to a tf-idf representation
-    X_tfidf =  tfizer.fit_transform(X_cnt).toarray()
-
-
-    feature_names = list(enumerate(cntizer.get_feature_names()))
-    feature_names
-
     type_indicators = [ "IE: Introversion (I) / Extroversion (E)", "NS: Intuition (N) – Sensing (S)", 
                     "FT: Feeling (F) - Thinking (T)", "JP: Judging (J) – Perceiving (P)"  ]
 
 
-
     # Posts in tf-idf representation
     X = X_tfidf
-
-    # setup parameters for xgboost
-    param = {}
-    param['n_estimators'] = 200
-    param['max_depth'] = 2
-    param['nthread'] = 8
-    param['learning_rate'] = 0.2
-
-
-    # Let's train type indicator individually
-    for l in range(len(type_indicators)):
-        #print("%s ..." % (type_indicators[l]))
-        
-        Y = list_personality[:,l]
-        
-        model = XGBClassifier(**param)
-        
-        param_grid = {
-            'n_estimators' : [ 200, 300],
-            'learning_rate': [ 0.2, 0.3]
-            # 'learning_rate': [ 0.01, 0.1, 0.2, 0.3],
-            # 'max_depth': [2,3,4],
-        }
-        
-        
-        kfold = StratifiedKFold(n_splits=10, shuffle=True, random_state=7)
-        grid_search = GridSearchCV(model, param_grid, scoring="neg_log_loss", n_jobs=-1, cv=kfold)
-        grid_result = grid_search.fit(X, Y)
-
-        # summarize results
-        #print("* Best: %f using %s" % (grid_result.best_score_, grid_result.best_params_))
-        means = grid_result.cv_results_['mean_test_score']
-        stds = grid_result.cv_results_['std_test_score']
-        params = grid_result.cv_results_['params']
-
 
 
 
@@ -246,8 +220,8 @@ def mbti_classify(text):
     result = []
     # Let's train type indicator individually
     for l in range(len(type_indicators)):
-        #print("%s ..." % (type_indicators[l]))
-        
+        print("%s ..." % (type_indicators[l]))
+
         Y = list_personality[:,l]
 
         # split data into train and test sets
@@ -258,23 +232,23 @@ def mbti_classify(text):
         # fit model on training data
         model = XGBClassifier(**param)
         model.fit(X_train, y_train)
-        
+
         # make predictions for my  data
         y_pred = model.predict(my_X_tfidf)
         result.append(y_pred[0])
-        # #print("* %s prediction: %s" % (type_indicators[l], y_pred))
+        # print("* %s prediction: %s" % (type_indicators[l], y_pred))
 
-        #print("The result is: ", translate_back(result))
+        print("The result is: ", translate_back(result))
 
         #결과를 리스트에 담고
         Result_list = list(translate_back(result))
 
 
 
-    #mbit 결과값에 따라 내용 #print 하기
+    #mbit 결과값에 따라 내용 print 하기
     # read data
     # data = pd.read_csv('/Users/jongphilkim/Desktop/Django_WEB/essayfitaiproject/essayai/mbti_exp.csv') 
-    data = pd.read_csv('/essayai/mbti_exp.csv') 
+    data = pd.read_csv('./mbti/mbti_exp.csv') 
 
     #새로운 데이터프레임을 만들어서 계산된 값을 추가할 예정
     df2 = pd.DataFrame(index=range(0,4),columns=['Type', 'Explain'])
@@ -286,33 +260,19 @@ def mbti_classify(text):
         for j in range(0, len(data)):   
             if type == data.iloc[j,0]:
                 break
-                
+
         is_mbti = data.iloc[j,2]
-        
+
         df2.iloc[i, [0,1]] = [type, is_mbti]    
-        #print(df2)
+        print(df2)
+
 
     return df2
 
 
-
-# A few few tweets and blog post
-my_posts  = """Getting started with data science and applying machine learning has never been as simple as it is now. There are many free and paid online tutorials and courses out there to help you to get started. I’ve recently started to learn, play, and work on Data Science & Machine Learning on Kaggle.com. In this brief post, I’d like to share my experience with the Kaggle Python Docker image, which simplifies the Data Scientist’s life.
-Awesome #AWS monitoring introduction.
-HPE Software (now @MicroFocusSW) won the platinum reader's choice #ITAWARDS 2017 in the new category #CloudMonitoring
-Certified as AWS Certified Solutions Architect 
-Hi, please have a look at my Udacity interview about online learning and machine learning,
-Very interesting to see the  lessons learnt during the HP Operations Orchestration to CloudSlang journey. http://bit.ly/1Xo41ci 
-I came across a post on devopsdigest.com and need your input: “70% DevOps organizations Unhappy with DevOps Monitoring Tools”
-In a similar investigation I found out that many DevOps organizations use several monitoring tools in parallel. Senu, Nagios, LogStach and SaaS offerings such as DataDog or SignalFX to name a few. However, one element is missing: Consolidation of alerts and status in a single pane of glass, which enables fast remediation of application and infrastructure uptime and performance issues.
-Sure, there are commercial tools on the market for exactly this use case but these tools are not necessarily optimized for DevOps.
-So, here my question to you: In your DevOps project, have you encountered that the lack of consolidation of alerts and status is a real issue? If yes, how did you approach the problem? Or is an ChatOps approach just right?
-You will probably hear more and more about ChatOps - at conferences, DevOps meet-ups or simply from your co-worker at the coffee station. ChatOps is a term and concept coined by GitHub. It's about the conversation-driven development, automation, and operations.
-Now the question is: why and how would I, as an ops-focused engineer, implement and use ChatOps in my organization? The next question then is: How to include my tools into the chat conversation?
-Let’s begin by having a look at a use case. The Closed Looped Incidents Process (CLIP) can be rejuvenated with ChatOps. The work from the incident detection runs through monitoring until the resolution of issues in your application or infrastructure can be accelerated with improved, cross-team communication and collaboration.
-In this blog post, I am going to describe and share my experience with deploying HP Operations Manager i 10.0 (OMi) on HP Helion Public Cloud. An Infrastructure as a Service platform such as HP Helion Public Cloud Compute is a great place to quickly spin-up a Linux server and install HP Operations Manager i for various use scenarios. An example of a good use case is monitoring workloads across public clouds such as AWS and Azure.
-"""
+# my_posts  = """Describe a place or environment where you are perfectly content. What do you do or experience there, and why is it meaningful to you? 644 words out of 650 Gettysburg, a small town in the middle of Pennsylvania, was the sight of the largest, bloodiest battle in the Civil War. Something about these hallowed grounds draws me back every year for a three day camping trip with my family over Labor Day weekend. Every year, once school starts, I count the days until I take that three and half hour drive from Pittsburgh to Gettysburg. Each year, we leave after school ends on Friday and arrive in Gettysburg with just enough daylight to pitch the tents and cook up a quick dinner on the campfire. As more of the extended family arrives, we circle around the campfire and find out what is new with everyone. The following morning, everyone is up by nine and helping to make breakfast which is our best meal of the day while camping. Breakfast will fuel us for the day as we hike the vast battlefields. My Uncle Mark, my twin brother, Andrew, and I like to take charge of the family tour since we have the most passion and knowledge about the battle. I have learned so much from the stories Mark tells us while walking on the tours. Through my own research during these last couple of trips, I did some of the explaining about the events that occurred during the battle 150 years ago. My fondest experience during one trip was when we decided to go off of the main path to find a carving in a rock from a soldier during the battle. Mark had read about the carving in one of his books about Gettysburg, and we were determined to locate it. After almost an hour of scanning rocks in the area, we finally found it with just enough daylight to read what it said. After a long day of exploring the battlefield, we went back to the campsite for some 'civil war' stew. There is nothing special about the stew, just meat, vegetables and gravy, but for whatever reason, it is some of the best stew I have ever eaten. For the rest of the night, we enjoy the company of our extended family. My cousins, my brother and I listen to the stories from Mark and his friends experiences' in the military. After the parents have gone to bed, we stay up talking with each other, inching closer and closer to the fire as it gets colder. Finally, we creep back into our tents, trying to be as quiet as possible to not wake our parents. The next morning we awake red-eyed from the lack of sleep and cook up another fantastic breakfast. Unfortunately, after breakfast we have to pack up and head back to Pittsburgh. It will be another year until I visit Gettysburg again. There is something about that time I spend in Gettysburg that keeps me coming back to visit. For one, it is just a fun, relaxing time I get to spend with my family. This trip also fulfills my love for the outdoors. From sitting by the campfire and falling asleep to the chirp of the crickets, that is my definition of a perfect weekend. Gettysburg is also an interesting place to go for Civil War buffs like me. While walking down the Union line or walking Pickett's Charge, I imagine how the battle would have been played out around me. Every year when I visit Gettysburg, I learn more facts and stories about the battle, soldiers and generally about the Civil War. While I am in Gettysburg, I am perfectly content, passionate about the history and just enjoying the great outdoors with my family. This drive to learn goes beyond just my passion for history but applies to all of the math, science and business classes I have taken and clubs I am involved in at school. Every day, I am genuinely excited to learn.
+# """
 # test = mbti_classify(my_posts)
-# #print ('check') 
+# print ('check') 
 # test
-# #print ('check2') 
+# print ('check2') 
