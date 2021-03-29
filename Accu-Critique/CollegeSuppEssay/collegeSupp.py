46aefb2800851e2891e6977b86811a3d41368d4d
kwangilKyle <38448277+kwangilkimkenny@users.noreply.github.com>
2021-03-29 15:07:33 +0900

    Add files via upload

--- Accu-Critique/CollegeSuppEssay/collegeSupp.py
+++ Accu-Critique/CollegeSuppEssay/collegeSupp.py
@@ -1,5 +1,7 @@
+import numpy as np
 import spacy
 from collections import Counter
+import re
 import nltk
 nltk.download('averaged_perceptron_tagger')
 
@@ -25,125 +27,130 @@ def select_prompt_type(prompt_type):
     
     if prompt_type == 'why_us':
         pmt_typ = [""" 'Why us' school & major interest (select major, by college & department) """]
+        pmt_sentiment = ['Admiration', 'Excitement', 'Pride', 'Realization', 'Curiosity']
     elif prompt_type == 'Intellectual interest':
         pmt_typ = [""]
+        pmt_sentiment = ['Curiosity', 'Realization']
     elif prompt_type == 'Meaningful experience & lesson learned':
         pmt_typ = [""]
+        pmt_sentiment = ['']
     elif prompt_type ==  'Achievement you are proud of':
         pmt_typ = [""]
+        pmt_sentiment = ['Realization', 'Approval', 'Gratitude', 'Admiration', 'Pride', 'Desire', 'Optimism']
     elif prompt_type ==  'Social issues: contribution & solution':
         pmt_typ = [""]
+        pmt_sentiment = ['Anger', 'Fear', 'Disapproval','Disappointment','Realization','Approval', 'Gratitude', 'Admiration']
     elif prompt_type ==  'Summer activity':
         pmt_typ = [""]
+        pmt_sentiment = ['Pride','Realization','Curiosity','Excitement','Amusement','Caring']
     elif prompt_type ==  'Unique quality, passion, or talent':
         pmt_typ = [""]
+        pmt_sentiment = ['Pride','Excitement','Amusement','Approval','Admiration','Curiosity']
     elif prompt_type ==  'Extracurricular activity or work experience':
         pmt_typ = [""]
+        pmt_sentiment = ['Pride','Realization','Curiosity','Joy','Excitement','Amusement','Caring','Optimism']
     elif prompt_type ==  'Your community: role and contribution in your community':
         pmt_typ = [""]
+        pmt_sentiment = ['Admiration','Caring','Approval','Pride','Gratitude','Love']
     elif prompt_type ==  'College community: intended role, involvement, and contribution in college community':
         pmt_typ = [""]
+        pmt_sentiment = ['Admiration','Caring','Approval','Excitement','Pride','Gratitude']
     elif prompt_type ==  'Overcoming a Challenge or ethical dilemma':
         pmt_typ = [""]
+        pmt_sentiment = ['Anger','Fear','Disapproval','Disappointment','Confusion','Annoyed','Realization', 'Approval','Gratitude','Admiration','Relief','Optimism']
     elif prompt_type ==  'Culture & diversity':
         pmt_typ = [""]
+        pmt_sentiment = ['Admiration','Realization','Love','Approval','Pride','Gratitude']
     elif prompt_type ==  'Collaboration & teamwork':
         pmt_typ = [""]
+        pmt_sentiment = ['Admiration','Caring','Approval','Optimism','Gratitude','Love']
     elif prompt_type ==  'Creativity/creative projects':
         pmt_typ = [""]
+        pmt_sentiment = ['Excitement','Realization','Curiosity','Desire','Amusement','Surprise']
     elif prompt_type ==  'Leadership experience':
         pmt_typ = [""]
+        pmt_sentiment = ['Admiration','Caring','Approval','Optimism','Gratitude','Love','Fear','Confusion','Nervousness']
     elif prompt_type ==  'Values, perspectives, or beliefs':
         pmt_typ = [""]
+        pmt_sentiment = ['Anger','Fear','Disapproval','Disappointment','Realization','Approval','Gratitude','Admiration']
     elif prompt_type ==  'Person who influenced you':
         pmt_typ = [""]
+        pmt_sentiment = ['Realization', 'Approval', 'Gratitude','Admiration','Caring','Love','Curiosity', 'Pride', 'Joy']
     elif prompt_type ==  'Favorite book/movie/quote':
         pmt_typ = [""]
+        pmt_sentiment = ['Excitement', 'Realization', 'Curiosity','Admiration','Amusement','Joy']
     elif prompt_type ==  'Write to future roommate':
         pmt_typ = [""]
+        pmt_sentiment = ['Admiration','Realization','Love','Excitement','Approval','Pride','Gratitude','Amusement','Curiosity','Joy']
     elif prompt_type ==  'Diversity & Inclusion Statement':
         pmt_typ = [""]
+        pmt_sentiment = ['Anger','Fear','Disapproval','Disappointment','Confusion','Annoyed','Realization','Approval','Gratitude','Admiration','Relief','Optimism']
     elif prompt_type ==  'Future goals or reasons for learning':
         pmt_typ = [""]
+        pmt_sentiment = ['Realization','Approval','Gratitude','Admiration','Pride','Desire','Optimism']
     elif prompt_type ==  'What you do for fun':
         pmt_typ = [""]
-    else:
-        pass
-    
-
-# Selected Colege
-def selected_college(select_college):
-    if select_college == 'Stanford':
-        pass
-    elif select_college == 'Cambridge':
-        pass
-    elif select_college == 'Princeton':
-        pass
-    elif select_college == 'MIT':
-        pass
-    elif select_college == 'Yale':
-        pass
-    elif select_college == 'Penn':
-        pass
-    elif select_college == 'Chicago':
-        pass
-    elif select_college == 'Northwestern':
-        pass
-    elif select_college == 'Duke':
-        pass
-    elif select_college == 'Johns Hopkins':
-        pass
-    elif select_college == 'Caltech':
-        pass
-    elif select_college == 'Dartmouth':
-        pass
-    elif select_college == 'Brown':
-        pass
-    elif select_college == 'Notre Dame':
-        pass
-    elif select_college == 'Vanderbilt':
-        pass
-    elif select_college == 'Cornell':
-        pass
-    elif select_college == 'Rice':
-        pass
-    elif select_college == 'WUSTL':
-        pass
-    elif select_college == 'UCLA':
-        pass
-    elif select_college == 'Emory':
-        pass
-    elif select_college == 'UC Berkeley':
-        pass
-    elif select_college == 'USC':
-        pass
-    elif select_college == 'Georgetown':
-        pass
-    elif select_college == 'Carnegie Mellon':
-        pass
-    elif select_college == 'UVA':
-        pass
+        pmt_sentiment = ['Admiration', 'Excitement', 'Curiosity', 'Amusement', 'Pride','Joy']
     else:
         pass
 
+    # pmt_typ : prompt type 
+    # pmt_sentiment : prompy type에 해당하는 sentiment
+    return pmt_typ, pmt_sentiment
 
 
+#데이터 전처리 
+def cleaning(data):
+    fin_data = []
+    for data_itm in data:
+        # 영문자 이외 문자는 공백으로 변환
+        only_english = re.sub('[^a-zA-Z]', ' ', data_itm)  
+        only_english = only_english.rstrip('\n')
+        # 데이터를 리스트에 추가 
+        fin_data.append(only_english)
 
-# 에세이 입력
-input_text = """Bloomington Normal is almost laughably cliché for a midwestern city. Vast swathes of corn envelop winding roads and the heady smell of BBQ smoke pervades the countryside every summer. Yet, underlying the trite norms of Normal is the prescriptive force of tradition—the expectation to fulfill my role as a female Filipino by playing Debussy in the yearly piano festival and enrolling in multivariable calculus instead of political philosophy.So when I discovered the technical demand of bebop, the triplet groove, and the intricacies of chordal harmony after ten years of grueling classical piano, I was fascinated by the music's novelty. Jazz guitar was not only evocative and creative, but also strangely liberating. I began to explore different pedagogical methods, transcribe solos from the greats, and experiment with various approaches until my own unique sound began to develop. And, although I did not know what would be the 'best' route for me to follow as a musician, the freedom to forge whatever path I felt was right seemed to be exactly what I needed; there were no expectations for me to continue in any particular way—only the way that suited my own desires.While journeying this trail, I found myself at Interlochen Arts Camp the summer before my junior year. Never before had I been immersed in an environment so conducive to musical growth: I was surrounded by people intensely passionate about pursuing all kinds of art with no regard for ideas of what art 'should' be. I knew immediately that this would be a perfect opportunity to cultivate my sound, unbounded by the limits of confining tradition. On the first day of camp, I found that my peer guitarist in big band was another Filipino girl from Illinois. Until that moment, my endeavors in jazz guitar had been a solitary effort; I had no one with whom to collaborate and no one against whom I could compare myself, much less someone from a background mirroring my own. I was eager to play with her, but while I quickly recognized a slew of differences between us—different heights, guitars, and even playing styles—others seemed to have trouble making that distinction during performances. Some even went as far as calling me 'other-Francesca.' Thus, amidst the glittering lakes and musky pine needles of Interlochen, I once again confronted Bloomington's frustrating expectations.After being mistaken for her several times, I could not help but view Francesca as a standard of what the 'female Filipino jazz guitarist' should embody. Her improvisatory language, comping style and even personal qualities loomed above me as something I had to live up to. Nevertheless, as Francesca and I continued to play together, it was not long before we connected through our creative pursuit. In time, I learned to draw inspiration from her instead of feeling pressured to follow whatever precedent I thought she set. I found that I grew because of, rather than in spite of, her presence; I could find solace in our similarities and even a sense of comfort in an unfamiliar environment without being trapped by expectation. Though the pressure to conform was still present—and will likely remain present in my life no matter what genre I'm playing or what pursuits I engage in—I learned to eschew its corrosive influence and enjoy the rewards that it brings. While my encounter with Francesca at first sparked a feeling of pressure to conform in a setting where I never thought I would feel its presence, it also carried the warmth of finding someone with whom I could connect. Like the admittedly trite conditions of my hometown, the resemblances between us provided comfort to me through their familiarity. I ultimately found that I can embrace this warmth while still rejecting the pressure to succumb to expectations, and that, in the careful balance between these elements, I can grow in a way that feels both like discove"""
+    return fin_data
+
+# txt 문서 정보 불러오기 : 대학정보
+def open_data(select_college):
+    # 폴더 구조, 대학이름 입력 명칭을 통일해야 함
+    file_path = "./college_info/colleges_dataset/"
+    college_name = select_college
+    file_name = "_college_general_info.txt"
+    # file = open("./college_info/colleges_dataset/brown_college_general_info.txt", 'r')
+    file = open(file_path + college_name + file_name, 'r')
+    lists = file.readlines()
+    file.close()
+    lists_re =  cleaning(lists) # 영어 단어가 아닌 것은 삭제
+    result = ' '.join(lists_re)
+    return result
 
 
+# txt 문서 정보 불러오기 : 선택한 전공관련 정보 추출 
+# 입력값은 대학, 전공 ex) 'Browon', 'AfricanStudies'
+def open_major_data(select_college, select_major):
+    # 폴더 구조, 대학이름 입력 명칭을 통일해야 함
+    file_path = "./major_info/major_dataset/"
+    college_name = select_college
+    mjr_name = select_major
+    file_name = "_major_info.txt"
+    # file = open("./major_info/major_dataset/Brown_AfricanStudies_major_info.txt", 'r')
+    file = open(file_path + college_name + "_" + mjr_name + file_name, 'r')
+    lists = file.readlines()
+    file.close()
+    doc = ' '.join(lists)
+    return lists
 
 
-# 대학관련 정보의 토픽 키워드
+# 대학관련 정보의 토픽 키워드 추출하여 WordCloud로 구현
 def general_keywords(College_text_data):
     tokenized = nltk.word_tokenize(str(College_text_data))
-    print('tokenized:', tokenized)
+    #print('tokenized:', tokenized)
     nouns = [word for (word, pos) in nltk.pos_tag(tokenized) if(pos[:2] == 'NN')]
     count = Counter(nouns)
     words = dict(count.most_common())
-    print('words:', words)
-    # 가장 많이 등장하는 단어를 추려보자. 얏 그런데 ii가 많네. 이건 선언문이라 그러함. 이런 단어는 모두 전처리에서 삭제할것!!!
+    #print('words:', words)
+    # 가장 많이 등장하는 단어를 추려보자. 
     wordcloud = WordCloud(background_color='white',colormap = "Accent_r",
                             width=1500, height=1000).generate_from_frequencies(words)
 
@@ -153,7 +160,103 @@ def general_keywords(College_text_data):
 
     return gk_re
 
-## run ##
-result =  general_keywords(input_text)
-print("====================================")
-print('result :', result)
+
+# Selected College
+# 입력값:  대학, 전공 ex) 'why_us', 'Browon', 'AfricanStudies'
+def selected_college(select_pmt_type, select_college, select_major):
+
+    pmt_sent_etc_re = select_prompt_type(select_pmt_type)
+    prompt_type_sentence = pmt_sent_etc_re[0] # prompt 문장 ex) Prompt Type : Intellectual interest 이렇게 20가지가 있음
+    pmt_sent_re = list(pmt_sent_etc_re[1]) # prompt 에 해당하는 sentiment list
+    intended_mjr = select_major # 희망전공
+
+    if select_college == 'Harvard':
+        pass
+    elif select_college == 'Princeton':
+        pass
+    elif select_college == 'Stanford':
+        pass
+    elif select_college == 'MIT':
+        pass
+    elif select_college == 'Columbia':
+        pass
+    elif select_college == 'UPenn':
+        pass
+    elif select_college == 'Brown':
+        College_text_data = open_data(select_college) # 선택한 대학의 정보가 담긴 txt 파일을 불러오고
+        re_mjr = open_major_data(select_college, select_major) # 선택한 대학과 전공의 정보를 불러와서
+        gen_keywd_college = general_keywords(College_text_data) # 키워드 추출하여 대학정보 WordCloud로 구현
+        gen_keywd_college_major = general_keywords(re_mjr) # 키워드 추출하여 대학의 전공 WordCloud로 구현
+
+    elif select_college == 'Cornell':
+        pass
+    elif select_college == 'Dartmouth':
+        pass
+    elif select_college == 'UChicago':
+        pass
+    elif select_college == 'Northwestern':
+        pass
+    elif select_college == 'Duke':
+        pass
+    elif select_college == 'Johns Hopkins':
+        pass
+    elif select_college == 'UCLA':
+        pass
+    elif select_college == 'UC Berkeley':
+        pass
+    elif select_college == 'Carnegie Mellon':
+        pass
+    elif select_college == 'Emory':
+        pass
+    elif select_college == 'Georgetown':
+        pass
+    elif select_college == 'UCLA':
+        pass
+    elif select_college == 'Emory':
+        pass
+    elif select_college == 'Caltech':
+        pass
+    elif select_college == 'USC':
+        pass
+    elif select_college == 'Georgetown':
+        pass
+    elif select_college == 'Willams':
+        pass
+    elif select_college == 'Swarthmore':
+        pass
+    elif select_college == 'Amherst':
+        pass
+    else:
+        pass
+    
+    # 0. gen_keywd_college : 선택한 대학의 General Keywords on college로 wordcloud로 출력됨
+    # 1. gen_keywd_college_major : 선택 대학의 전공에 대한 keywords 를 WrodCloud 로 출력
+    # 2. intended_mjr : intended major
+    # 3. pmt_sent_etc_re : 선택한 prompt 질문
+    # 4. prompt_type_sentence : 선택한 prompt에 해당하는 질문 문장 전체
+    # 5. pmt_sent_re : 선택한 prompt에 해당하는 sentiment 리스트
+
+    data_result = {
+        'gen_keywd_college' : gen_keywd_college, # 선택한 대학의 General Keywords on college로 wordcloud로 출력됨
+        'gen_keywd_college_major' : gen_keywd_college_major, # 선택 대학의 전공에 대한 keywords 를 WrodCloud 로 출력
+        'intended_mjr' : intended_mjr, #intended major
+        'pmt_sent_etc_re' : pmt_sent_etc_re, #선택한 prompt 질문
+        'prompt_type_sentence' : prompt_type_sentence, #선택한 prompt에 해당하는 질문 문장 전체
+        'pmt_sent_re' : pmt_sent_re # 선택한 prompt에 해당하는 sentiment 리스트
+    }
+
+    return data_result
+
+
+
+### 실행 ###
+# 입력값은 대학, 전공 ex) 'why_us', 'Browon', 'African Studies'
+
+sc_re = selected_college('why_us', 'Brown', 'African Studies')
+print('Select_College:', sc_re)
+
+# 에세이 입력
+#input_text = """Bloomington Normal is almost laughably cliché for a midwestern city. Vast swathes of corn envelop winding roads and the heady smell of BBQ smoke pervades the countryside every summer. Yet, underlying the trite norms of Normal is the prescriptive force of tradition—the expectation to fulfill my role as a female Filipino by playing Debussy in the yearly piano festival and enrolling in multivariable calculus instead of political philosophy.So when I discovered the technical demand of bebop, the triplet groove, and the intricacies of chordal harmony after ten years of grueling classical piano, I was fascinated by the music's novelty. Jazz guitar was not only evocative and creative, but also strangely liberating. I began to explore different pedagogical methods, transcribe solos from the greats, and experiment with various approaches until my own unique sound began to develop. And, although I did not know what would be the 'best' route for me to follow as a musician, the freedom to forge whatever path I felt was right seemed to be exactly what I needed; there were no expectations for me to continue in any particular way—only the way that suited my own desires.While journeying this trail, I found myself at Interlochen Arts Camp the summer before my junior year. Never before had I been immersed in an environment so conducive to musical growth: I was surrounded by people intensely passionate about pursuing all kinds of art with no regard for ideas of what art 'should' be. I knew immediately that this would be a perfect opportunity to cultivate my sound, unbounded by the limits of confining tradition. On the first day of camp, I found that my peer guitarist in big band was another Filipino girl from Illinois. Until that moment, my endeavors in jazz guitar had been a solitary effort; I had no one with whom to collaborate and no one against whom I could compare myself, much less someone from a background mirroring my own. I was eager to play with her, but while I quickly recognized a slew of differences between us—different heights, guitars, and even playing styles—others seemed to have trouble making that distinction during performances. Some even went as far as calling me 'other-Francesca.' Thus, amidst the glittering lakes and musky pine needles of Interlochen, I once again confronted Bloomington's frustrating expectations.After being mistaken for her several times, I could not help but view Francesca as a standard of what the 'female Filipino jazz guitarist' should embody. Her improvisatory language, comping style and even personal qualities loomed above me as something I had to live up to. Nevertheless, as Francesca and I continued to play together, it was not long before we connected through our creative pursuit. In time, I learned to draw inspiration from her instead of feeling pressured to follow whatever precedent I thought she set. I found that I grew because of, rather than in spite of, her presence; I could find solace in our similarities and even a sense of comfort in an unfamiliar environment without being trapped by expectation. Though the pressure to conform was still present—and will likely remain present in my life no matter what genre I'm playing or what pursuits I engage in—I learned to eschew its corrosive influence and enjoy the rewards that it brings. While my encounter with Francesca at first sparked a feeling of pressure to conform in a setting where I never thought I would feel its presence, it also carried the warmth of finding someone with whom I could connect. Like the admittedly trite conditions of my hometown, the resemblances between us provided comfort to me through their familiarity. I ultimately found that I can embrace this warmth while still rejecting the pressure to succumb to expectations, and that, in the careful balance between these elements, I can grow in a way that feels both like discove"""
+
+
+
