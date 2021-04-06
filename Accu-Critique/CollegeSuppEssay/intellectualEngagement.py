024ba102942810a1d384d6868aad69de2574c9b3
kwangilKyle <38448277+kwangilkimkenny@users.noreply.github.com>
2021-04-06 16:15:44 +0900

    Add files via upload

---
+++ Accu-Critique/CollegeSuppEssay/intellectualEngagement.py
@@ -0,0 +1,5 @@
+# Major Fit 20%
+# General Academic Topics 30%
+# Prompt Oriented Sentiment 20%
+# Intellectual Engagement 20%(Cohesion level 40% + Academic Verbs 60%)
+# ref: https://docs.google.com/document/d/1P0EP7-T4AL-btgIrOPUOeSaQ8qOmcoRpMEZXIgIcOBU/edit
