792331cf397e9d7d70e639e71c22d8c8e5df3638
kwangilKyle <38448277+kwangilkimkenny@users.noreply.github.com>
2021-04-10 12:33:55 +0900

    intellectual engagement 개발 완료 etc
    Preprcessing code도 완성하였음. 나중에 활용하시오.
    intellectual engagement 부분완성하였음!!

--- Accu-Critique/CollegeSuppEssay/intellectualEngagement.py
+++ Accu-Critique/CollegeSuppEssay/intellectualEngagement.py
@@ -185,19 +185,24 @@ def getCoherenceLevel(essay_input):
 
 
     final_topics = [fin_topic_re]
-    #print('final_topics:', final_topics)
+    print('final_topics:', final_topics)
+
+    # Coherece를 계산하기위한 최종 추출한 토픽의 수로 fin_topics_number이 몇개가 나왔을 때 Cohesion Score가 계산되는 구조
+    fin_topics_number = len(fin_topic_re)
 
     # coherence value
     cm = CoherenceModel(topics=final_topics, corpus=corpus, dictionary=dictionary, coherence='u_mass')
-    coherence = cm.get_coherence()
+    coherence = round(cm.get_coherence(), 2)
 
     #print('Choerence Value:', coherence)
 
-    #### getAcademicVerbs(essay_input) ####
-    academic_verbs_ratio = getAcademicVerbs(essay_input)
+    # 결과설명
+    # fin_topics_number :  Coherece를 계산하기위한 최종 추출한 토픽의 총수
+    # coherence : Cohesion Score로 최종계산값
+    # topics : 에세이에서 추출한 토픽 ---------------> 웹페이지에 표시할 문자열 적용
 
-    return coherence, academic_verbs_ratio
-    # Choerence Value: -20.458978939301947  이렇게 추출됨, 의미는 높으면 일관성있게 문장을 작성했다는 의미, 낮으면 내용이 산만하다는 의미
+    return fin_topics_number, coherence, topics
+    # Choerence Value: -20.45  이렇게 추출됨, 의미는 높으면 일관성있게 문장을 작성했다는 의미, 낮으면 내용이 산만하다는 의미
 
 
 
@@ -232,10 +237,8 @@ def getAcademicVerbs(essay_input):
 
     #print('preprossed_sent_all:', preprossed_sent_all)
     
-     # preprossed_sent_all 이중 리스트를 flatten하게 만들고, 여기에서 Academic Verbs를 카운트해서 비교 계산하면 됨
+    # preprossed_sent_all 이중 리스트를 flatten하게 만들고, 여기에서 Academic Verbs를 카운트해서 비교 계산하면 됨
     flatten_dic_list = [y for x in preprossed_sent_all for y in x]
-
-
 
     #Academic Verbs
     academicVerbs = ['diagnose','Typifies','play','Reaches','Focus','compose','list','contradict','Occur','regulates','Expounds','Licenses',
@@ -277,16 +280,105 @@ def getAcademicVerbs(essay_input):
                         'Hints','Alleviates','Compensate','Outlaws','feature','maximize','Deviate','Delivers','Specify','increment','Contribute','Scrutinizes','Thwarts',
                         'Coordinate','Argue','Identifies','Precede','rebuts','Upholds','cease','Deduct','corroborate','Diminish','Sparks','establish']
     
+    # 개별 문자로 비교하기 위해서 소문자로 모두 변환
+    lower_academicVerbs = [i.lower() for i in academicVerbs]
+
+    # 단어 비교 flatten_dic_list vs lower_academicVerbs
+    # flatten_dic_list : 입력한 에세이이 문장에서의 동사
+    # lower_academicVerbs : 아카데믹 동사
+    ext_used_acdemic_verbs_list = []
+    for a in lower_academicVerbs:
+        if a in flatten_dic_list:
+            ext_used_acdemic_verbs_list.append(a)
+
+    # 전체 문장에서 아카데믹단어 사용 비율 계산
+    academic_verbs_usage_ration = round((len(ext_used_acdemic_verbs_list) / len(flatten_dic_list)) * 100, 2)
     
-    return flatten_dic_list
+    return academic_verbs_usage_ration
 
 
+# Intellectual Engagement 20%(Cohesion level 40% + Academic Verbs 60%)
+def intellectualEnguagement(essay_input):
+    #### Cohesion ####
+    get_coherence_score= getCoherenceLevel(essay_input)
+    #print('Result of Coherence Score', get_coherence_score)
 
+    # 웹페이지 표시할 문자열
+    intellectualEnguagement_words_for_web = get_coherence_score[2]
+
+    #### getAcademicVerbs(essay_input) ####
+    academic_verbs_ratio = getAcademicVerbs(essay_input) 
+    #print('Cohesion Score :', academic_verbs_ratio) #  by UMass Measure
+
+    # 이 비율을 적용해서 계산할 것 Cohesion level 40% + Academic Verbs 60%)
+
+    ###################### 합격생들의 평균 값 ########################
+    # coherence 절대값 변환
+    coherece_score_abs = abs(get_coherence_score[1])
+    # 비교하기 1
+    if coherece_score_abs >= 14: 
+        cohe_score = 30 # 너무 분산되어 있다.
+    elif coherece_score_abs >=7 and coherece_score_abs < 14: 
+        cohe_score = 80 # 적당히 분산되어 있다.
+    elif coherece_score_abs >= 3 and coherece_score_abs < 7:
+        cohe_score = 50 # 다소 집중되어있다.
+    else: # < 3 이하로 매우 집중되어있다.
+        cohe_score = 30
+
+    coherece_comp_result = cohe_score * 0.4 # 40% 적용
+    #print('최종 Coherence Score :', coherece_comp_result)
+
+    # 비교하기 2
+    if academic_verbs_ratio >= 15: # 15% 전체문장에서 사용했다면, 많이 사용했네
+        aca_score = 90
+    elif academic_verbs_ratio < 15 and academic_verbs_ratio >= 10: # 약간 많이 사용
+        aca_score = 60
+    elif academic_verbs_ratio < 10 and academic_verbs_ratio >= 5: # 조금 적게 사용, 덜 아카데믹함
+        aca_score = 30
+    else: # 5 이하로 사용 매우 적게 사용했음
+        aca_score = 10
+
+    academic_comp_result = aca_score * 0.6
+    #print('최종 Acacdemic Verbs Usg Result:', academic_comp_result)
+
+    # 최종곟산
+    intell_eng_result = coherece_comp_result + academic_comp_result
+
+    # 5단계로 계산(최대 80*0.4=32, 90*0.6=54 로 90점 만점으로 5단계로 나누면 18점 차이로 구분할 것)
+    if intell_eng_result >=72:
+        int_eng_re = 'Supurb'
+    elif intell_eng_result < 72 and intell_eng_result >= 54:
+        int_eng_re = 'Strong'
+    elif intell_eng_result < 54 and intell_eng_result >= 36:
+        int_eng_re = 'Good'
+    elif intell_eng_result < 36 and intell_eng_result >= 18:
+        int_eng_re = 'Mediocre'
+    else:
+        int_eng_re = 'Lacking'
+
+    return intell_eng_result, int_eng_re, intellectualEnguagement_words_for_web
 
 #### run ####
 
 # input College Supp Essay 
 essay_input = """I inhale deeply and blow harder than I thought possible, pushing the tiny ember from its resting place on the candle out into the air. The room erupts around me, and 'Happy Birthday!' cheers echo through the halls. It's time to make a wish. In my mind, that new Limited Edition Deluxe Ben 10 watch will soon be mine. My parents and the aunties and uncles around me attempt to point me in a different direction. 'Wish that you get to go to the temple every day when you're older! Wish that you memorize all your Sanskrit texts before you turn 6! Wish that you can live in India after college!' My ears listen, but my mind tunes them out, as nothing could possibly compare to that toy watch! What I never realized on my third birthday is that those wishes quietly tell the story of how my family hopes my life will play out. In this version of my life, there wasn't much room for change, personal growth, or 'rocking the boat.' A vital aspect of my family's cultural background is their focus on accepting things as they are. Growing up, I was discouraged from questioning others or asking questions that didn't have definitive yes or no answers. If I innocently asked my grandma why she expected me to touch her feet, my dad would grab my hand in a sudden swoop, look me sternly in the eye, and tell me not to disrespect her like that again. At home, if I mentioned that I had tried eggs for breakfast at a friend's house, I'd be looked at like I had just committed a felony for eating what my parents considered meat. If I asked the priest at the temple why he had asked an Indian man and his white wife to leave, I'd be met with a condescending glare and told that I should also leave for asking such questions.In direct contrast, my curiosity was invited and encouraged at school. After an environmental science lesson, I stayed for a few minutes after class to ask my 4th-grade science teacher with wide eyes how it was possible that Niagara Falls doesn't run out of flowing water. Instead of scolding me for asking her a 'dumb question,' she smiled and explained the intricacy of the water cycle. Now, if a teacher mentions that we'll learn about why a certain proof or idea works only in a future class, I'll stay after to ask more or pour through an advanced textbook to try to understand it. While my perspective was widening at school, the receptiveness to raising complex questions at home was diminishing. After earning my driver's license, I registered as an organ donor. My small checkmark on a piece of paper led to an intense clash between my and my parents' moral platform. I wanted to ensure that I positively contributed to society, while my parents believed that organ donation was an unfamiliar and unnecessary cultural taboo. I would often ask for clarity or for reasons that supported their ideologies. Their response would usually entail feeling a deep, visceral sense that traditions must be followed exactly as taught, without objection. Told in one language to keep asking questions and in another to ask only the right ones, I chose exploring questions that don't have answers, rather than accepting answers that don't get questioned. When it comes to the maze of learning, even when I take a wrong turn and encounter roadblocks that are meant to stop me, I've learned to climb over them and keep moving forward. My curiosity strengthens with each hurdle and has expanded into a pure love of learning new things. I've become someone who seeks to understand things at a fundamental level and who finds excitement in taking on big questions that have yet to be solved. I'm no longer afraid to rock the boat. "},{"index":1,"personal_essay":"Ever since I first held a small foam Spiderman basketball in my tiny hands and watched my idol Kobe Bryant hit every three-pointer he attempted, I've wanted to understand and replicate his flawless jump shot. As my math education progressed in school, I began to realize I had the tools to create a perfect shot formula. After learning about variables for the first time in 5th grade Algebra, I began to treat each aspect of Kobe's jump shot as a different variable, each combination of variables resulting in a unique solution. While in 7th-grade geometry, I graphed the arc of his shot, and after learning about quadratic equations in 8th grade, I expressed his shot as a parabolic function that would ensure a swish when shooting from any spot. After calculus lessons in 10th and 11th grade, I was excited to finally solve for the perfect velocity and acceleration needed on my release. At Brown, I hope to explore this intellectual pursuit through a different lens. What if I could maximize the odds of making shots if I understood the science behind one's mental mindset and focus through CLPS 500: Perception and Action? Or use astrophysics to account for drag and gravitational force anywhere in the universe? Or use data science to break down the analytics of the NBA's best shooters? Through the Open Curriculum, I see myself not only becoming a more complete learner, but also a more complete thinker, applying a flexible mindset to any problem I encounter. Brown's Open Curriculum allows students to explore broadly while also diving deeply into their academic pursuits. Tell us about an academic interest (or interests) that excites you, and how you might use the Open Curriculum to pursue it. I've been playing the Mridangam since I was five years old. It's a simple instrument: A wood barrel covered on two ends by goatskin with leather straps surrounding the hull. This instrument serves as a connection between me and one of the most beautiful aspects of my culture: Carnatic music. As a young child, I'd be taken to the temple every weekend for three-hour-long Carnatic music concerts, where the most accomplished teenagers and young adults in our local Indian community would perform. I would watch in awe as the mridangists' hands moved gracefully, flowing across the goatskin as if they weren't making contact, while simultaneously producing sharp rhythmic patterns that never failed to fall on the beat. Hoping to be like these idols on the stage, I trained intensely with my teacher, a strict man who taught me that the simple drum I was playing had thousands of years of culture behind it. Building up from simple strokes, I realized that the finger speed I'd had been awestruck by wasn't some magical talent, it was instead a science perfected by repeated practice."""
 
-get_coherence_level = getCoherenceLevel(essay_input)
-print('Result of Coherence Level:', get_coherence_level)
+# 입력 : 에세이(essay_input)를 입력하면 됨
+result = intellectualEnguagement(essay_input)
+print('Intellectual Engagement:', result)
+
+# 결과설명
+
+# intell_eng_result : 최종 계산한 intellectual Enguagement Result
+# int_eng_re : 5가지 구분 항목중 하나의 값을 계산 추출 (Supurb ~ lacking)
+# intellectualEnguagement_words_for_web : 웹에 표시할 단어 리스트(의 원형값들)로 에세이에 포함된 Academic 관련한 단어들
+
+### 결과는 다음과 같음 ###
+#Intellectual Engagement: (18.0, 'Mediocre', ['ideologies', 'love', 'support', 'variables', 'india', 'reason', 'complete', 'register', 'time', 'rock', 'question', 'instrument', 'echo', 'learn', 'cheer', 'science', 'young', 'aunties', 'shoot', 'academic', 'mridangam', 'years', 'small', 'boat', 'progress', 'strengthen', 'aspect', 'pure', 'wish', 'halls', 'pursue', 'clarity', 'different', 'birthday', 'excite', 'play', 'grade'])
+
+
+
+### Coherence에 대한 부연설명 ##
+# fin_topics_number :  Coherece를 계산하기위한 최종 추출한 토픽의 총수   -- 여기서는 4가 나왔음.
+# coherence : Cohesion Score로 최종계산값   --- 여기서는 -20.46으로 0에 가까워질수록 완벽하게 응집력이 있다고 본다. 이 값은 u_mass 로 계산한 Coherence 값이다.
+#             Coherence는 0일때 완벽한 응집성, 하지만 내용이 너무 협소한 관점으로 다루어져있고, 너무 커지면 주제가 분산되어 있다, -14 ~14 기준으로 본다. 여기서 -20은 내용이 산만하다는 의미다.
